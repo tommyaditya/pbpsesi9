bc9f3a887054960f74992eee1fc2a55c0b1e2878
tommyaditya <160828804+tommyaditya@users.noreply.github.com>
2024-12-21 09:47:21 -0800

    user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -9,4 +9,22 @@ const getUserById = async (id) => {
     const [rows] = await db.query('SELECT * FROM users WHERE id=?', id);
     return rows;
 }
-module.exports = { getAllUsers , getUserById };
+
+const addUser = async (user) => {
+    const { name, email, phone } = user;
+    const [result] = await db.query('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)', [name, email, phone]);
+    return result.insertId;
+}
+
+const updateUserById = async (id, user) => {
+    const { name, email, phone } = user;
+    const [result] = await db.query('UPDATE users SET name=?, email=?, phone=? WHERE id=?', [name, email, phone, id]);
+    return result.affectedRows;
+}
+
+const deleteUserById = async (id) => {
+    const [result] = await db.query('DELETE FROM users WHERE id=?', id);
+    return result.affectedRows;
+}
+
+module.exports = { getAllUsers , getUserById , addUser, updateUserById, deleteUserById };
