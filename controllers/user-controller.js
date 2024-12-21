2775ae7ff078b918128cd2808870bd008e3f6997
tommyaditya <160828804+tommyaditya@users.noreply.github.com>
2024-12-21 09:46:10 -0800

    user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -23,4 +23,40 @@ const getUserById = async (req, res) => {
         res.json({ message: 'Error Get User By Id' });
     }
 }
-module.exports = { getAllUsers , getUserById };
+
+const addUser = async (req, res) => {
+    try {
+        const newUserId = await userModel.addUser(req.body);
+        res.status(200).json({id: newUserId,...req.body});
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error Insert Data', error });
+    }
+}
+
+const updateUserById = async (req, res) => {
+    try {
+        const user = await userModel.updateUserById(req.params.id, req.body);
+        if(!user) res.json({ message: 'User Not Found' });
+        res.json(user);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error Update User By Id' });
+    }
+}
+
+const deleteUserById = async (req, res) => {
+    try {
+        const user = await userModel.deleteUserById(req.params.id);
+        if(!user) res.json({ message: 'User Not Found' });
+        res.json(user);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error Delete User By Id' });
+    }
+}
+
+module.exports = { getAllUsers , getUserById, addUser, updateUserById, deleteUserById };
