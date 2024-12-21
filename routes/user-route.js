59e34c5920f5602dbfb3a8f6ed9aae0e01fd9578
tommyaditya <160828804+tommyaditya@users.noreply.github.com>
2024-12-21 09:47:59 -0800

    user-route.js

--- routes/user-route.js
+++ routes/user-route.js
@@ -4,5 +4,8 @@ const router = express.Router();
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
+router.post('/', userController.addUser);
+router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
 
 module.exports = router;
